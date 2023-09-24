e1f29d85c172b44e5b2c79c356578fa41ddaee00
David Godoy <dgodoyrodri@gmail.com>
2023-09-24 21:41:53 +0200

    Added transcriptions to the solution, create Cohere connector

--- force-app/main/default/lwc/classifyCall/classifyCall.js
+++ force-app/main/default/lwc/classifyCall/classifyCall.js
@@ -1,13 +1,25 @@
 import { LightningElement, api } from 'lwc';
+import classifyCall from "@salesforce/apex/CallClassificationController.classifyCall";
 
 export default class ClassifyCall extends LightningElement {
+    loading = false;
 
     @api
     initializeItem(itemData) {
-        // TODO - Call to the backend
+        this.loading = true;
         
-
-
+        classifyCall({ transcribedAudioList: itemData })
+        .then((result) => {
+          this.transcribedAudioList = result;
+          this.categorizeSpeakerSections();
+        })
+        .catch((error) => {
+          this.error = error;
+          console.log(error);
+        })
+        .finally(() => {
+            this.loading = false;
+        });
 
     }
 
--- force-app/main/default/lwc/transcribeAudio/transcribeAudio.js
+++ force-app/main/default/lwc/transcribeAudio/transcribeAudio.js
@@ -1,13 +1,15 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import queryAudioTranscription from "@salesforce/apex/CallClassificationController.queryAudioTranscription";
 
 export default class TranscribeAudio extends LightningElement {
     loading = false;
+    transcribedAudioList;
+    @track transcribedAudioSectionsList;    
     error;
 
     @api
     getItemDataHook() {
-        return ;
+      return this.transcribedAudioList;
     }
 
     @api
@@ -17,7 +19,8 @@ export default class TranscribeAudio extends LightningElement {
 
         queryAudioTranscription({ contentDocumentId: contentDocumentId })
         .then((result) => {
-          this.setOptions(result);
+          this.transcribedAudioList = result;
+          this.categorizeSpeakerSections();
         })
         .catch((error) => {
           this.error = error;
@@ -26,6 +29,25 @@ export default class TranscribeAudio extends LightningElement {
         .finally(() => {
             this.loading = false;
         });
+    }
+
+    categorizeSpeakerSections() {
+      this.transcribedAudioSectionsList = [];
+      this.transcribedAudioList.forEach((transcribedAudioSection) => {
+        if (transcribedAudioSection.includes('Speaker 0:')) {
+          this.transcribedAudioSectionsList.push({
+            'inbound': false,
+            'outbound': true, 
+            'text': transcribedAudioSection.replace('Speaker 0:', '')
+          });
+        } else if (transcribedAudioSection.includes('Speaker 1:')) {
+          this.transcribedAudioSectionsList.push({
+            'inbound': true, 
+            'outbound': false,
+            'text': transcribedAudioSection.replace('Speaker 1:', '')
+          });
+        }
+      });
     }
 
 }
