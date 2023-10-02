30aa0eeafdaa981fb8c10915801b95e7b63bbb3b
David Godoy <dgodoyrodri@gmail.com>
2023-10-02 19:23:51 +0200

    Switch to detailed model. Show record to insert in the LWC

--- force-app/main/default/lwc/classifyCall/classifyCall.js
+++ force-app/main/default/lwc/classifyCall/classifyCall.js
@@ -3,35 +3,45 @@ import classifyCall from "@salesforce/apex/CallClassificationController.classify
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ClassifyCall extends LightningElement {
-    loading = false;
+  loading = false;
+  contacts;
+  opportunities;
+  products;
+  tasks;
 
-    @api
-    initializeItem(itemData) {
-        this.loading = true;
-        
-        classifyCall({ transcribedAudioList: itemData })
-        .then((result) => {
-          // this.transcribedAudioList = result;
-          // TODO - Create sections of objects to insert in Salesforce
-        })
-        .catch((error) => {
-          this.error = error;
-          console.log(error);
-          this.showToast(error);
-        })
-        .finally(() => {
-            this.loading = false;
-        });
-
-    }
-
-    showToast(error) {
-      const event = new ShowToastEvent({
-          title: 'Toast Error',
-          message: error.body.message,
-          variant: 'error'
+  @api
+  initializeItem(itemData) {
+      this.loading = true;
+      
+      classifyCall({ transcribedAudioList: itemData })
+      .then((result) => {
+        this.contacts = result.contacts;
+        this.opportunities = result.opportunities;
+        this.products = result.products;
+        this.tasks = result.tasks;
+      })
+      .catch((error) => {
+        this.error = error;
+        console.log(error);
+        this.showToast(error);
+      })
+      .finally(() => {
+          this.loading = false;
       });
-      this.dispatchEvent(event);
+
   }
 
+  showToast(error) {
+    const event = new ShowToastEvent({
+        title: 'Toast Error',
+        message: error.body.message,
+        variant: 'error'
+    });
+    this.dispatchEvent(event);
+  }
+
+  getUniqueKey() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  }
+  
 }
